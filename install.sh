b4ec52c97fe5e7d763e58d1930961e2c56953a1c
Harris Marfel <hrace009@gmail.com>
2017-05-28 05:09:00 +0700

    Fix some minor bugs

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Download, build and install wine 32-bit on CentOS 7
 
 # For details of this script, please check
-# http://www.systutorials.com/239913/install-32-bit-wine-1-8-centos-7/
+# https://www.systutorials.com/239913/install-32-bit-wine-1-8-centos-7/
 
 # Author: Eric Zhiqiang Ma (zma@ericzma.com)
 
@@ -13,7 +13,7 @@ log=`mktemp -t install-wine.XXXXXX.log`
 
 # Install
 
-ver=1.8.4
+ver=1.8.5
 
 echo "Hello there. Start to download, build and install wine $ver 32-bit and 64-bit versions..." | tee $log
 echo "Logs are in $log" | tee -a $log
@@ -27,7 +27,7 @@ echo "Install wine building tools..." | tee -a $log
 yum install samba-winbind-clients -y 2>&1 >>$log
 yum groupinstall 'Development Tools' -y 2>&1 >> $log
 yum install libjpeg-turbo-devel libtiff-devel freetype-devel -y 2>&1 >> $log
-yum install libgcc.i686 libX11-devel.i686 freetype-devel.i686 gnutls-devel.i686 libxml2-devel.i686 libjpeg-turbo-devel.i686 libpng-devel.i686 libXrender-devel.i686 -y 2>&1 >> $log
+yum install glibc-devel.{i686,x86_64} libgcc.{i686,x86_64} libX11-devel.{i686,x86_64} freetype-devel.{i686,x86_64} gnutls-devel.{i686,x86_64} libxml2-devel.{i686,x86_64} libjpeg-turbo-devel.{i686,x86_64} libpng-devel.{i686,x86_64} libXrender-devel.{i686,x86_64} alsa-lib-devel.{i686,x86_64} -y 2>&1 >> $log
 
 echo "Download and unpack the wine source package..." 2>&1 | tee -a $log
 
@@ -41,12 +41,12 @@ mkdir -p wine32 wine64 2>&1 >> $log
 
 echo "   build wine64..." 2>&1 | tee -a $log
 cd wine64 2>&1 >> $log
-../configure --without-x --enable-win64 2>&1 >> $log
+../configure --enable-win64 2>&1 >> $log
 make -j 4 2>&1 >> $log
 
 echo "   build wine32..." 2>&1 | tee -a $log
 cd ../wine32 2>&1 >> $log
-../configure --without-x --with-wine64=../wine64 2>&1 >> $log
+../configure --with-wine64=../wine64 2>&1 >> $log
 make -j 4 2>&1 >> $log
 
 echo "Install wine..." 2>&1 | tee -a $log
@@ -61,7 +61,8 @@ echo "Congratulation! All are done. Enjoy!" 2>&1 | tee -a $log
 rm -f $log
 
 # # Uninstall
-cd /usr/src/wine-${ver}/wine32
-make uninstall
-cd /usr/src/wine-${ver}/wine64
-make uninstall
+# cd /usr/src/wine-${ver}/wine32
+# make uninstall
+# cd /usr/src/wine-${ver}/wine64
+# make uninstall
+
